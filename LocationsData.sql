504be372c114356ee61493775e59aca57c01b53e
xsxb <ruoyu100@163.com>
2025-01-27 08:29:01 -0500

    Added locations table data

---
+++ LocationsData.sql
@@ -0,0 +1,44 @@
+use mmsDB;
+
+/*
+Drei PCs  O-PC-1/2, CH-PC-1
+USB-Speichermedien fangen mit USB an
+ */
+INSERT INTO locations (label) VALUES 
+( 'O-SH-1' ),
+( 'O-SH-2' ),
+( 'O-SH-3' ),
+( 'O-SH-4' ),
+( 'O-PC-1' ),
+( 'O-PC-2' ),
+( 'O-DRAWER-1' ),
+( 'O-DRAWER-2' ),
+( 'O-DRAWER-3' ),
+( 'O-DRAWER-4' ),
+( 'S-SH-1' ),
+( 'S-SH-2' ),
+( 'S-BOX-1' ),
+( 'S-BOX-2' ),
+( 'S-BOX-3' ),
+( 'S-BOX-4' ),
+( 'S-BOX-5' ),
+( 'S-BOX-6' ),
+( 'S-BOX-7' ),
+( 'S-BOX-8' ),
+( 'CE-BOX-1' ),
+( 'CE-BOX-2' ),
+( 'CE-BOX-3' ),
+( 'CE-BOX-4' ),
+( 'CE-BOX-5' ),
+( 'CE-BOX-6' ),
+( 'CE-BOX-7' ),
+( 'CE-BOX-8' ),
+( 'CH-BOX-1' ),
+( 'CH-PC-1' ),
+( 'CH-SH-1' ),
+( 'CH-SH-2' ),
+( 'USB-HDD-Toshiba 1TB'),
+( 'USB-SSD-Samsung 512GB'),
+( 'USB-Stick-Lukas-Blau'),
+( 'USB-Stick-Alex-ScanDisk')
+;
