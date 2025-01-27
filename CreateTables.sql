96303925d244a41913514c16185d0e19cb0443fd
unknown <ITAD1-TN09@ITAD.LOCAL>
2025-01-27 15:37:44 +0100

    mediaID no auto increment

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,23 +1,30 @@
 CREATE DATABASE IF NOT EXISTS mmsDB;
+
 CREATE TABLE IF NOT EXISTS media (
-	mediaID int NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-	title nvarchar (255), 
-	releaseDate date, 
-	addedDate date, 
+
+	mediaID INT PRIMARY KEY,
+	title VARCHAR(255) NOT NULL,
+	releaseDate DATE, 
+	addedAt DATETIME NOT NULL,
 	digital BOOL
+	
 	);
 
 CREATE TABLE IF NOT EXISTS locations (
-	locationID int AUTO_INCREMENT PRIMARY KEY,
-	label varchar(255) NOT NULL
+	
+	locationID INT AUTO_INCREMENT PRIMARY KEY,
+	label VARCHAR(255) NOT NULL
+	
 	);
 
 CREATE TABLE IF NOT EXISTS med_at_loc (
-	locationID int NOT NULL,
-	mediaID int NOT NULL,
+	
+	locationID INT NOT NULL,
+	mediaID INT NOT NULL,
 	FOREIGN KEY (locationID) REFERENCES locations(locationID),
 	FOREIGN KEY (mediaID) REFERENCES media(mediaID),
 	PRIMARY KEY (locationID, mediaID)
+	
 	);
 
 
