ade042fb329831046afcd27e31ff15848a8a86e9
Ekaterina.S <83958308+EkaterinaShmeleva@users.noreply.github.com>
2025-01-28 09:41:37 +0100

    Update to InsertData
    nach mediaID sortiert, Attribut genre bei Seien ergänzt

--- InsertData.sql
+++ InsertData.sql
@@ -1,4 +1,29 @@
 USE mmsDB;
+-- Einfügen von Daten in die Tabelle media für films
+INSERT INTO media (mediaID, title, releaseDate, addedDate, digital) VALUES
+(1, 'Inception', '2010-07-16', '2025-01-27', TRUE),
+(2, 'The Matrix', '1999-03-31', '2025-01-27', TRUE),
+(3, 'Interstellar', '2014-11-07', '2025-01-27', FALSE),
+(4, 'The Godfather', '1972-03-24', '2025-01-27', FALSE),
+(5, 'Pulp Fiction', '1994-10-14', '2025-01-27', TRUE),
+(6, 'The Dark Knight', '2008-07-18', '2025-01-27', TRUE),
+(7, 'Fight Club', '1999-10-15', '2025-01-27', FALSE),
+(8, 'Forrest Gump', '1994-07-06', '2025-01-27', FALSE),
+(9, 'The Shawshank Redemption', '1994-09-23', '2025-01-27', TRUE),
+(10, 'The Lord of the Rings: The Fellowship of the Ring', '2001-12-19', '2025-01-27', TRUE);
+
+-- Einfügen von Daten in die Tabelle films
+INSERT INTO films (mediaID, length, director, genre, rating_imdb) VALUES
+(1, 148, 'Christopher Nolan', 'Sci-Fi', 8.8),
+(2, 136, 'Lana Wachowski, Lilly Wachowski', 'Sci-Fi', 8.7),
+(3, 169, 'Christopher Nolan', 'Sci-Fi', 8.6),
+(4, 175, 'Francis Ford Coppola', 'Crime', 9.2),
+(5, 154, 'Quentin Tarantino', 'Crime', 8.9),
+(6, 152, 'Christopher Nolan', 'Action', 9.0),
+(7, 139, 'David Fincher', 'Drama', 8.8),
+(8, 142, 'Robert Zemeckis', 'Drama', 8.8),
+(9, 142, 'Frank Darabont', 'Drama', 9.3),
+(10, 178, 'Peter Jackson', 'Fantasy', 8.8);
 
 -- Einfügen von Daten in die Tabelle media für music
 INSERT INTO media (mediaID, title, releaseDate, addedDate, digital) VALUES
@@ -26,32 +51,15 @@ INSERT INTO music (mediaID, artist, genre, album) VALUES
 (19, 'Prince', 'Pop', 'Purple Rain'),
 (20, 'Adele', 'Pop', '21');
 
-
--- Einfügen von Daten in die Tabelle media für films
+-- Einfügen von Daten in die Tabelle media für books
 INSERT INTO media (mediaID, title, releaseDate, addedDate, digital) VALUES
-(1, 'Inception', '2010-07-16', '2025-01-27', TRUE),
-(2, 'The Matrix', '1999-03-31', '2025-01-27', TRUE),
-(3, 'Interstellar', '2014-11-07', '2025-01-27', FALSE),
-(4, 'The Godfather', '1972-03-24', '2025-01-27', FALSE),
-(5, 'Pulp Fiction', '1994-10-14', '2025-01-27', TRUE),
-(6, 'The Dark Knight', '2008-07-18', '2025-01-27', TRUE),
-(7, 'Fight Club', '1999-10-15', '2025-01-27', FALSE),
-(8, 'Forrest Gump', '1994-07-06', '2025-01-27', FALSE),
-(9, 'The Shawshank Redemption', '1994-09-23', '2025-01-27', TRUE),
-(10, 'The Lord of the Rings: The Fellowship of the Ring', '2001-12-19', '2025-01-27', TRUE);
-
--- Einfügen von Daten in die Tabelle films
-INSERT INTO films (mediaID, length, director, genre, rating_imdb) VALUES
-(1, 148, 'Christopher Nolan', 'Sci-Fi', 8.8),
-(2, 136, 'Lana Wachowski, Lilly Wachowski', 'Sci-Fi', 8.7),
-(3, 169, 'Christopher Nolan', 'Sci-Fi', 8.6),
-(4, 175, 'Francis Ford Coppola', 'Crime', 9.2),
-(5, 154, 'Quentin Tarantino', 'Crime', 8.9),
-(6, 152, 'Christopher Nolan', 'Action', 9.0),
-(7, 139, 'David Fincher', 'Drama', 8.8),
-(8, 142, 'Robert Zemeckis', 'Drama', 8.8),
-(9, 142, 'Frank Darabont', 'Drama', 9.3),
-(10, 178, 'Peter Jackson', 'Fantasy', 8.8);
+(21, 'Wo ich wohne ist, der Mond ganz nah', '2024-01-11', '2025-01-27', FALSE),
+(22, 'Der Herr der Ringe', '2019-08-24', '2025-01-27', FALSE),
+(23, 'Die Säulen der Erde', '2000-01-01', '2025-01-27', FALSE),
+(24, 'Ilias Odyssee', '2002-09-01', '2025-01-27', FALSE),
+(25, 'Dreizehn lustige Erzählungen', '1992-01-01', '2025-01-27', FALSE),
+(26, 'Ein Sommernachtstraum', '1998-04-01', '2025-01-27', FALSE),
+(27, 'Schuld und Sühne', '1997-07-01', '2025-01-27', FALSE);
 
 -- Einfügen von Daten in die Tabelle books
 INSERT INTO books (mediaID, author) VALUES
@@ -62,16 +70,6 @@ INSERT INTO books (mediaID, author) VALUES
 (25, 'Anton Tschechow'),
 (26, 'William Shakespeare'),
 (27, 'Fjodor M. Dostojewski');
-
--- Einfügen von Daten in die Tabelle media für books
-INSERT INTO media (mediaID, title, releaseDate, addedDate, digital) VALUES
-(21, 'Wo ich wohne ist, der Mond ganz nah', '2024-01-11', '2025-01-27', FALSE),
-(22, 'Der Herr der Ringe', '2019-08-24', '2025-01-27', FALSE),
-(23, 'Die Säulen der Erde', '2000-01-01', '2025-01-27', FALSE),
-(24, 'Ilias Odyssee', '2002-09-01', '2025-01-27', FALSE),
-(25, 'Dreizehn lustige Erzählungen', '1992-01-01', '2025-01-27', FALSE),
-(26, 'Ein Sommernachtstraum', '1998-04-01', '2025-01-27', FALSE),
-(27, 'Schuld und Sühne', '1997-07-01', '2025-01-27', FALSE);
 
 -- Einfügen von Daten in die Tabelle media für series
 INSERT INTO media (mediaID, title, releaseDate, addedDate, digital) VALUES
@@ -87,15 +85,14 @@ INSERT INTO media (mediaID, title, releaseDate, addedDate, digital) VALUES
 (40, 'Chernobyl', '2019-05-06', '2025-01-27', TRUE);
 
 -- Einfügen von Daten in die Tabelle series
-INSERT INTO series (mediaID, seasons) VALUES
-(31, 8),
-(32, 5),
-(33, 9),
-(34, 7),
-(35, 6),
-(36, 10),
-(37, 4),
-(38, 3),
-(39, 2),
-(40, 1);
-
+INSERT INTO series (mediaID, seasons, genre) VALUES
+(31, 8, 'Fantasy'),
+(32, 5, 'Crime'),
+(33, 9, 'Comedy'),
+(34, 7, 'Comedy'),
+(35, 6, 'Crime'),
+(36, 10, 'Animation'),
+(37, 4, 'Sci-Fi'),
+(38, 3, 'Sci-Fi'),
+(39, 2, 'Comedy'),
+(40, 1, 'Drama');
