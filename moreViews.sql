459e911976bdf0a319845c015f9ca36dd4e5599a
unknown <ITAD1-TN09@ITAD.LOCAL>
2025-01-28 14:15:28 +0100

    comment syntax fix and added IF NOT EXISTSs

--- moreViews.sql
+++ moreViews.sql
@@ -1,7 +1,7 @@
---View for all media titles and their types:
+-- View for all media titles and their types:
 USE mmsdb;
 
-CREATE VIEW media_titles AS
+CREATE VIEW IF NOT EXISTS media_titles AS
 SELECT media.mediaID, media.title, 
        CASE 
            WHEN music.mediaID IS NOT NULL THEN 'Music'
@@ -15,32 +15,32 @@ LEFT JOIN films ON media.mediaID = films.mediaID
 LEFT JOIN books ON media.mediaID = books.mediaID
 LEFT JOIN series ON media.mediaID = series.mediaID;
 
---View for all books with their authors:
+-- View for all books with their authors:
 
-CREATE VIEW book_details AS
+CREATE VIEW IF NOT EXISTS book_details AS
 SELECT media.mediaID, media.title, books.author, media.releaseDate
 FROM media
 JOIN books ON media.mediaID = books.mediaID;
 
 
---View for all films with their directors and IMDb ratings:
+-- View for all films with their directors and IMDb ratings:
 
-CREATE VIEW film_details AS
+CREATE VIEW IF NOT EXISTS film_details AS
 SELECT media.mediaID, media.title, films.director, films.rating_imdb
 FROM media
 JOIN films ON media.mediaID = films.mediaID;
 
 
---View for all series with their genres and number of seasons:
+-- View for all series with their genres and number of seasons:
 
-CREATE VIEW series_details AS
+CREATE VIEW IF NOT EXISTS series_details AS
 SELECT media.mediaID, media.title, series.genre, series.seasons
 FROM media
 JOIN series ON media.mediaID = series.mediaID;
 
---View for all music with their artists and albums:
+-- View for all music with their artists and albums:
 
-CREATE VIEW music_details AS
+CREATE VIEW IF NOT EXISTS music_details AS
 SELECT media.mediaID, media.title, music.artist, music.album
 FROM media
 JOIN music ON media.mediaID = music.mediaID;
