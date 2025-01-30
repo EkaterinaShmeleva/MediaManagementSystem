a9c02a0c87778c52df72f90d6faebf988892e1b4
Ekaterina.S <83958308+EkaterinaShmeleva@users.noreply.github.com>
2025-01-30 08:25:39 +0100

    Update moreViews (view_viewName)
    Die Namen von Views geändert (view_viewName)

--- moreViews.sql
+++ moreViews.sql
@@ -1,8 +1,8 @@
 -- View for all media titles and their types:
 USE mmsDB;
 
-DROP VIEW IF EXISTS media_titles;
-CREATE VIEW IF NOT EXISTS media_titles AS
+DROP VIEW IF EXISTS view_media_titles;
+CREATE VIEW IF NOT EXISTS view_media_titles AS
 SELECT media.mediaID, media.title, 
        CASE 
            WHEN music.mediaID IS NOT NULL THEN 'Music'
@@ -18,8 +18,8 @@ LEFT JOIN series ON media.mediaID = series.mediaID;
 
 -- View for all books with their authors:
 
-DROP VIEW IF EXISTS book_details; 
-CREATE VIEW IF NOT EXISTS book_details AS
+DROP VIEW IF EXISTS view_book_details; 
+CREATE VIEW IF NOT EXISTS view_book_details AS
 SELECT media.mediaID, media.title, books.author, media.releaseDate, media.digital
 FROM media
 JOIN books ON media.mediaID = books.mediaID;
@@ -27,8 +27,8 @@ JOIN books ON media.mediaID = books.mediaID;
 
 -- View for all films with their directors and IMDb ratings:
 
-DROP VIEW IF EXISTS film_details; 
-CREATE VIEW IF NOT EXISTS film_details AS
+DROP VIEW IF EXISTS view_film_details; 
+CREATE VIEW IF NOT EXISTS view_film_details AS
 SELECT media.mediaID, media.title, films.director, films.rating_imdb, media.digital
 FROM media
 JOIN films ON media.mediaID = films.mediaID;
@@ -36,16 +36,16 @@ JOIN films ON media.mediaID = films.mediaID;
 
 -- View for all series with their genres and number of seasons:
 
-DROP VIEW IF EXISTS series_details; 
-CREATE VIEW IF NOT EXISTS series_details AS
+DROP VIEW IF EXISTS view_series_details; 
+CREATE VIEW IF NOT EXISTS view_series_details AS
 SELECT media.mediaID, media.title, series.genre, series.seasons, media.digital
 FROM media
 JOIN series ON media.mediaID = series.mediaID;
 
 -- View for all music with their artists and albums:
 
-DROP VIEW IF EXISTS music_details; 
-CREATE VIEW IF NOT EXISTS music_details AS
+DROP VIEW IF EXISTS view_music_details; 
+CREATE VIEW IF NOT EXISTS view_music_details AS
 SELECT media.mediaID, media.title, music.artist, music.album, media.digital
 FROM media
 JOIN music ON media.mediaID = music.mediaID;
